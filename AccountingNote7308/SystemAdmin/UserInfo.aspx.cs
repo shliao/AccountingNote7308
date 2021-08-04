57f876feeda46e511c4744c0aef06f4f8bfc9ce3
jeff <jeff123433@hotmail.com>
2021-08-04 16:56:37 +0800

    [建立] 新增使用者資訊頁&會員變更密碼頁

--- AccountingNote7308/SystemAdmin/UserInfo.aspx.cs
+++ AccountingNote7308/SystemAdmin/UserInfo.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Accounting.dbSource;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,34 @@ namespace AccountingNote7308.SystemAdmin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!this.IsPostBack)
+            {
+                if (this.Session["UserLoginInfo"] == null)
+                {
+                    Response.Redirect("/Login.aspx");
+                    return;
+                }
 
+                string account = this.Session["UserLoginInfo"] as string;
+                DataRow dr = UserInfoManager.GetUserInfobyAccount(account);
+
+                if (dr == null)
+                {
+                    this.Session["UserLoginInfo"] = null;
+                    Response.Redirect("/Login.aspx");
+                    return;
+                }
+
+                this.ltAccount.Text = dr["Account"].ToString();
+                this.ltName.Text = dr["Name"].ToString();
+                this.ltEmail.Text = dr["Email"].ToString();
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Session["UserLoginInfo"] = null;
+            Response.Redirect("/Login.aspx");
         }
     }
 }
--- AccountingNote7308/SystemAdmin/UserPassword.aspx.cs
+++ AccountingNote7308/SystemAdmin/UserPassword.aspx.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Accounting.dbSource;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +15,114 @@ namespace AccountingNote7308.SystemAdmin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (this.Session["UserLoginInfo"] == null)
+            {
+                Response.Redirect("/Login.aspx");
+                return;
+            }
 
+            string account = this.Session["UserLoginInfo"] as string;
+            DataRow dr = UserInfoManager.GetUserInfobyAccount(account);
+
+            if (dr == null)
+            {
+                this.Session["UserLoginInfo"] = null;
+                Response.Redirect("/Login.aspx");
+                return;
+            }
+
+            this.ltAccount.Text = dr["Account"].ToString();
+        }
+
+        protected void btnChange_Click(object sender, EventArgs e)
+        {
+            string account = this.Session["UserLoginInfo"] as string;
+            DataRow dr = UserInfoManager.GetUserInfobyAccount(account);
+
+            if (dr == null)
+            {
+                this.Session["UserLoginInfo"] = null;
+                Response.Redirect("/Login.aspx");
+                return;
+            }
+
+            this.ltAccount.Text = dr["Account"].ToString();
+
+            string a = dr["PWD"].ToString();
+            string b = txbPWD.Text;
+
+            if (a == b)
+            {
+                this.txbPWD.Text = dr["PWD"].ToString();
+            }
+            else
+            {
+                this.txbPWD.Text = string.Empty;
+                Response.Redirect("UserPassword.aspx");
+                return;
+            }
+
+            string c = txbNewPWD.Text;
+            string d = txbNewPWD_Check.Text;
+
+            if (c == d)
+            {
+                UpdatePWD(dr["Account"].ToString(), d);
+                this.Session["UserLoginInfo"] = null;
+                Response.Redirect("/Login.aspx");
+                return;
+            }
+            else
+            {
+                this.txbNewPWD.Text = string.Empty;
+                this.txbNewPWD_Check.Text = string.Empty;
+                Response.Redirect("UserPassword.aspx");
+                return;
+            }
+        }
+
+        public static bool UpdatePWD(string Account, string PWD)
+        {
+            string connStr = GetConnectionString2();
+            string dbCommand =
+                $@"UPDATE [UserInfo]
+                   SET
+                      PWD = @pwd
+                  WHERE
+                      Account = @account
+                ";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand comm = new SqlCommand(dbCommand, conn))
+                {
+                    comm.Parameters.AddWithValue("@account", Account);
+                    comm.Parameters.AddWithValue("@pwd", PWD);
+                    
+                    try
+                    {
+                        conn.Open();
+                        int effectRows = comm.ExecuteNonQuery();
+
+                        if (effectRows == 1)
+                            return true;
+                        else
+                            return false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.Writelog(ex);
+                        return false;
+                    }
+                }
+            }
+        }
+
+        public static string GetConnectionString2()
+        {
+            //string val = ConfigurationManager.AppSettings["ConnectionString"];
+            string val2 = ConfigurationManager.ConnectionStrings["Default Connection"].ConnectionString;
+            return val2;
         }
     }
 }
