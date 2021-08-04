57b7cca5eca95f547e0c8fb1a7c93dcd214763c1
thousandtw <h0916916145@gmail.com>
2021-08-04 16:16:39 +0800

    Login&Default更改

--- AccountingNote7308/Login.aspx.cs
+++ AccountingNote7308/Login.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AccountingNote.Auth;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,35 @@ namespace AccountingNote7308
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (this.Session["UserLoginInfo"] != null)
+            {
+                this.plcLogin.Visible = false;
+                Response.Redirect("SystemAdmin / Userinfo.aspx");
+            }
+            else
+            {
+                this.plcLogin.Visible = true;
+            }
+        }
 
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string inp_Account = this.txtAccount.Text; 
+            string inp_PWD = this.txtPWD.Text;
+
+            string msg;
+            if (!AuthManager.tryLogin(inp_Account, inp_PWD, out msg))
+            {
+                this.ltlMsg.Text = msg;
+                return;
+            }
+
+            Response.Redirect("/SystemAdmin/UserInfo.aspx");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
